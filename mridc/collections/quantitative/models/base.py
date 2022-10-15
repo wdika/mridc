* fixes deepsource, mypy issues * adds weighting option for dice loss

mridc/collections/quantitative/models/base.py
@@ -664,15 +664,13 @@ class BaseqMRIReconstructionModel(base_reconstruction_models.BaseMRIReconstructi
         if self.use_reconstruction_module:
             for echo_time in range(target.shape[1]):  # type: ignore
                 self.log_image(
-                    # type: ignore
                     f"{key}/reconstruction_echo_{echo_time}/target",
-                    target[:, echo_time, :, :],
+                    target[:, echo_time, :, :],  # type: ignore
                 )  # type: ignore
                 self.log_image(f"{key}/reconstruction_echo_{echo_time}/reconstruction", recon_pred[:, echo_time, :, :])
                 self.log_image(
                     f"{key}/reconstruction_echo_{echo_time}/error",
-                    # type: ignore
-                    torch.abs(target[:, echo_time, :, :] - recon_pred[:, echo_time, :, :]),
+                    torch.abs(target[:, echo_time, :, :] - recon_pred[:, echo_time, :, :]),  # type: ignore
                 )
 
         self.log_image(f"{key}/R2star/target", R2star_map_target)
@@ -969,15 +967,13 @@ class BaseqMRIReconstructionModel(base_reconstruction_models.BaseMRIReconstructi
         if self.use_reconstruction_module:
             for echo_time in range(target.shape[1]):  # type: ignore
                 self.log_image(
-                    # type: ignore
                     f"{key}/reconstruction_echo_{echo_time}/target",
-                    target[:, echo_time, :, :],
+                    target[:, echo_time, :, :],  # type: ignore
                 )  # type: ignore
                 self.log_image(f"{key}/reconstruction_echo_{echo_time}/reconstruction", recon_pred[:, echo_time, :, :])
                 self.log_image(
                     f"{key}/reconstruction_echo_{echo_time}/error",
-                    # type: ignore
-                    torch.abs(target[:, echo_time, :, :] - recon_pred[:, echo_time, :, :]),
+                    torch.abs(target[:, echo_time, :, :] - recon_pred[:, echo_time, :, :]),  # type: ignore
                 )
 
         self.log_image(f"{key}/R2star/target", R2star_map_target)

mridc/collections/quantitative/parts/transforms.py
@@ -246,8 +246,7 @@ class qMRIDataTransforms:
 
         if self.apply_prewhitening:
             kspace = torch.stack(
-                # type: ignore
-                [self.prewhitening(kspace[echo]) for echo in range(kspace.shape[0])],
+                [self.prewhitening(kspace[echo]) for echo in range(kspace.shape[0])],  # type: ignore
                 dim=0,
             )
 
@@ -870,8 +869,7 @@ class GaussianSmoothing(torch.nn.Module):
         # The gaussian kernel is the product of the gaussian function of each dimension.
         kernel = 1
         meshgrids = torch.meshgrid(
-            # type: ignore
-            [torch.arange(size, dtype=torch.float32) for size in kernel_size],
+            [torch.arange(size, dtype=torch.float32) for size in kernel_size],  # type: ignore
             indexing="ij",
         )
         for size, std, mgrid in zip(kernel_size, sigma, meshgrids):  # type: ignore
@@ -895,7 +893,7 @@ class GaussianSmoothing(torch.nn.Module):
         elif dim == 3:
             self.conv = F.conv3d
         else:
-            raise RuntimeError("Only 1, 2 and 3 dimensions are supported. Received {}.".format(dim))
+            raise RuntimeError(f"Only 1, 2 and 3 dimensions are supported. Received {dim}.")
 
     def forward(self, input):
         """

mridc/collections/reconstruction/data/subsample.py
@@ -496,11 +496,9 @@ class Poisson2DMaskFunc(MaskFunc):
 
         ny, nx = self.shape
         y, x = np.mgrid[:ny, :nx]
-        # type: ignore
-        x = np.maximum(abs(x - self.shape[-1] / 2) - calib[-1] / 2, 0)
+        x = np.maximum(abs(x - self.shape[-1] / 2) - calib[-1] / 2, 0)  # type: ignore
         x /= x.max()
-        # type: ignore
-        y = np.maximum(abs(y - self.shape[-2] / 2) - calib[-2] / 2, 0)
+        y = np.maximum(abs(y - self.shape[-2] / 2) - calib[-2] / 2, 0)  # type: ignore
         y /= y.max()
         r = np.sqrt(x**2 + y**2)
 

mridc/collections/segmentation/losses/dice.py
@@ -37,6 +37,7 @@ class Dice(_Loss):
         sigmoid: bool = True,
         softmax: bool = False,
         other_act: Optional[Callable] = None,
+        w_type: Optional[str] = None,
         squared_pred: bool = False,
         jaccard: bool = False,
         flatten: bool = False,
@@ -58,6 +59,9 @@ class Dice(_Loss):
             Whether to add softmax function to the input data. Defaults to False.
         other_act: Callable
             Use this parameter if you want to apply another type of activation layer.
+            Defaults to None.
+        w_type: str
+            Type of weight function to be used for calculating Dice loss. Defaults to "square".
         squared_pred: bool
             Whether to square the prediction before calculating Dice. Defaults to False.
         jaccard: bool
@@ -91,6 +95,7 @@ class Dice(_Loss):
         self.sigmoid = sigmoid
         self.softmax = softmax
         self.other_act = other_act
+        self.w_type = w_type
         self.squared_pred = squared_pred
         self.jaccard = jaccard
         self.flatten = flatten
@@ -99,14 +104,22 @@ class Dice(_Loss):
         self.smooth_dr = float(smooth_dr)
         self.batch = batch
 
+    def w_func(self, grnd):
+        """Weight function for Dice loss."""
+        if self.w_type == "simple":
+            return torch.reciprocal(grnd)
+        if self.w_type == "square":
+            return torch.reciprocal(grnd * grnd)
+        return torch.ones_like(grnd)
+
     def forward(self, target: torch.Tensor, input: torch.Tensor) -> Tuple[Union[Tensor, Any], Tensor]:
         """
         Parameters
         ----------
-        target: torch.Tensor
-            the ground truth of shape [BNHW[D]].
         input: torch.Tensor
             the prediction of shape [BNHW[D]].
+        target: torch.Tensor
+            the ground truth of shape [BNHW[D]].
         """
         if self.flatten:
             target = target.view(target.shape[0], 1, -1)
@@ -162,7 +175,20 @@ class Dice(_Loss):
         if self.jaccard:
             denominator = 2.0 * (denominator - intersection)
 
-        dice_score = (2.0 * intersection + self.smooth_nr) / (denominator + self.smooth_dr)
+        w = self.w_func(ground_o.float())
+        infs = torch.isinf(w)
+        if self.batch:
+            w[infs] = 0.0
+            w = w + infs * torch.max(w)
+        else:
+            w[infs] = 0.0
+            max_values = torch.max(w, dim=1)[0].unsqueeze(dim=1)
+            w = w + infs * max_values
+
+        final_reduce_dim = 0 if self.batch else 1
+        numer = 2.0 * (intersection * w).sum(final_reduce_dim, keepdim=True) + self.smooth_nr
+        denom = (denominator * w).sum(final_reduce_dim, keepdim=True) + self.smooth_dr
+        dice_score = numer / denom
         f: torch.Tensor = 1.0 - dice_score
 
         if self.reduction == "mean":

mridc/collections/segmentation/models/base.py
@@ -73,6 +73,7 @@ class BaseMRIJointReconstructionSegmentationModel(base_reconstruction_models.Bas
                 sigmoid=cfg_dict.get("dice_loss_sigmoid", True),
                 softmax=cfg_dict.get("dice_loss_softmax", False),
                 other_act=cfg_dict.get("dice_loss_other_act", None),
+                w_type=cfg_dict.get("dice_loss_w_type", None),
                 squared_pred=cfg_dict.get("dice_loss_squared_pred", False),
                 jaccard=cfg_dict.get("dice_loss_jaccard", False),
                 flatten=cfg_dict.get("dice_loss_flatten", False),
@@ -103,6 +104,7 @@ class BaseMRIJointReconstructionSegmentationModel(base_reconstruction_models.Bas
             sigmoid=cfg_dict.get("dice_metric_sigmoid", False),
             softmax=cfg_dict.get("dice_metric_softmax", True),
             other_act=cfg_dict.get("dice_metric_other_act", None),
+            w_type=cfg_dict.get("dice_loss_w_type", None),
             squared_pred=cfg_dict.get("dice_metric_squared_pred", False),
             jaccard=cfg_dict.get("dice_metric_jaccard", False),
             flatten=cfg_dict.get("dice_metric_flatten", False),
@@ -469,13 +471,8 @@ class BaseMRIJointReconstructionSegmentationModel(base_reconstruction_models.Bas
             val_loss = self.total_segmentation_loss_weight * segmentation_loss
 
         for class_idx in range(target_segmentation.shape[1]):  # type: ignore
-            # type: ignore
-            target_segmentation_class = target_segmentation[:, class_idx]
-            target_segmentation_class = target_segmentation_class / torch.max(torch.abs(target_segmentation_class))
-
+            target_segmentation_class = target_segmentation[:, class_idx]  # type: ignore
             output_segmentation_class = pred_segmentation[:, class_idx]
-            output_segmentation_class = output_segmentation_class / torch.max(torch.abs(output_segmentation_class))
-
             self.log_image(
                 f"{key}/segmentation_classes/target_class_{class_idx}",
                 target_segmentation_class,  # type: ignore
@@ -624,12 +621,8 @@ class BaseMRIJointReconstructionSegmentationModel(base_reconstruction_models.Bas
             ).view(1)
 
         for class_idx in range(target_segmentation.shape[1]):  # type: ignore
-            # type: ignore
-            target_segmentation_class = target_segmentation[:, class_idx]
-            target_segmentation_class = target_segmentation_class / torch.max(torch.abs(target_segmentation_class))
-
+            target_segmentation_class = target_segmentation[:, class_idx]  # type: ignore
             output_segmentation_class = pred_segmentation[:, class_idx]
-            output_segmentation_class = output_segmentation_class / torch.max(torch.abs(output_segmentation_class))
 
             self.log_image(
                 f"{key}/segmentation_classes/target_class_{class_idx}",

mridc/collections/segmentation/parts/transforms.py
@@ -248,8 +248,7 @@ class JRSMRIDataTransforms:
         if self.complex_data:
             if self.apply_prewhitening:
                 kspace = torch.stack(
-                    # type: ignore
-                    [self.prewhitening(kspace[echo]) for echo in range(kspace.shape[0])],
+                    [self.prewhitening(kspace[echo]) for echo in range(kspace.shape[0])],  # type: ignore
                     dim=0,
                 )
 
@@ -462,9 +461,9 @@ class JRSMRIDataTransforms:
                 acc = torch.tensor([1])
 
                 if mask is None or mask.ndim == 0:
-                    mask = torch.ones(masked_kspace.shape[-3], masked_kspace.shape[-2]).type(
+                    mask = torch.ones(masked_kspace.shape[-3], masked_kspace.shape[-2]).type(  # type: ignore
                         torch.float32
-                    )  # type: ignore
+                    )
                 else:
                     mask = torch.from_numpy(mask)
 