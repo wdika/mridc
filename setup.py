49abc292b603f2234022e380ef0704bde809fc3c
wdika <dimitriskarkalousos@gmail.com>
2022-12-05 05:22:41 +0100

    * updates testing python versions

--- setup.py
+++ setup.py
@@ -167,7 +167,6 @@ setuptools.setup(
         # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: Apache Software License",
         # Supported python versions
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
